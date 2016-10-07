ca63252cbfc391e6dbb95d37ec3b4a6b47944de2
Ettore Delprino <edelprino@gmail.com>
2016-10-07 16:03:39 +0200

    Added first test

---
+++ test/RequestBuilder.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+  var request =  {
+    "sender":"SENDER_ID",
+    "text":"prova",
+    "originalRequest":{
+      "token":"TOKEN_ID",
+      "team_id":"T024J9QH0",
+      "team_domain":"xpeppers",
+      "channel_id":"D02HRMAPF",
+      "channel_name":"directmessage",
+      "user_id":"SENDER_ID",
+      "user_name":"ettoredelprino",
+      "command":"/track",
+      "text":"prova",
+      "response_url":"https://hooks.slack.com/commands/T024J9QH0/88688115858/poPJLtcJvydwpzahL9hankv7"
+    },
+    "type":"slack-slash-command"
+  }
+
+  this.withText = function(text) {
+    request.originalRequest.text = text
+    request.text = text
+    return this
+  }
+
+  this.withUserName = function(userName) {
+    request.originalRequest.user_name = userName
+    return this
+  }
+
+  this.build = function() {
+    return request;
+  }
+
+}
---
+++ test/TrackTodayTest.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const sinon = require('sinon')
+const TrackerBot = require('../lib/TrackerBot')
+const RequestBuilder = require('./RequestBuilder')
+
+const tokenRepository = { findFromUsername: function(){} }
+
+
+describe('Track today command', function() {
+  var bot = new TrackerBot(tokenRepository)
+
+  it('return', function() {
+    response = bot(new RequestBuilder().withText("today").withUserName("ettoredelprino").build())
+
+    assert.equal("Ciao ettoredelprino. Il tuo token è undefined", response)
+  })
+})
