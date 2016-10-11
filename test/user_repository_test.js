2dd870ae8cac91820852eb9a846d026faca934bb
Ettore Delprino <edelprino@gmail.com>
2016-10-11 14:30:37 +0200

    Added user repository

--- test/user_repository_test.js
+++ test/user_repository_test.js
@@ -1,8 +1,11 @@
+const assert = require('assert')
 const userRepository = require('../lib/user_repository')
 
-
 describe('UserRepository', function() {
-  it('returns user object on given username', function() {
+  it('returns user object on given username', function(done) {
+    user = userRepository.findFromUsername('testuser', done)
 
+    assert.equal('test_token', user.token)
+    assert.equal('test_project', user.project)
   })
 })
