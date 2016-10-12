b7b6fd24ab6ec075526ede06eed396e498ca872c
Ettore Delprino <edelprino@gmail.com>
2016-10-12 15:27:51 +0200

    Promised tracker

--- test/tracker_test.js
+++ test/tracker_test.js
@@ -1,10 +1,15 @@
 'use strict'
 const assert = require('assert')
 const sinon = require('sinon')
+const TOKEN = 'any_token'
+const tracker = require('../lib/tracker')(TOKEN)
 
-describe('Tracker', function() {
-  it('track today', function() {
-
+describe('Tracker', () => {
+  it.skip('track today', () => {
+    return tracker.trackToday().then((entries) => {
+      assert.equal(entries.length, 2)
+      assert.equal(entries[1].description, 'Phoenix')
+      assert.equal(entries[0].description, 'Phoenix')
+    })
   })
 })
-
