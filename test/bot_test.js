7cb36240e21c679374397b8971908d2ea968ebd5
Ettore Delprino <edelprino@gmail.com>
2016-10-12 11:57:12 +0200

    Added policies for dynamodb

--- test/bot_test.js
+++ test/bot_test.js
@@ -1,26 +1,20 @@
 'use strict'
 const assert = require('assert')
-const sinon = require('sinon')
 const requestBuilder = require('./RequestBuilder')
-
 const bot = require('../lib/bot')
-const userRepository = require('../lib/user_repository')
 
 const ANY_USERNAME = 'username'
-const TOKEN = 'token'
-const USER_FROM_REPOSITORY = { token: TOKEN, username: ANY_USERNAME }
 
 describe('Bot', () => {
   it('returns message ok on track today command', () => {
-    const userRepositoryMock = sinon.mock(userRepository)
-    userRepositoryMock.expects("findFromUsername").once().withArgs(ANY_USERNAME).returns(USER_FROM_REPOSITORY)
     const request = requestBuilder().withText("today").withUserName(ANY_USERNAME)
 
     const response = bot(request)
 
-    userRepositoryMock.verify()
-    assert.equal('Ciao ' + ANY_USERNAME + '. Ho tracciato la giornata di oggi.', response)
-    userRepositoryMock.restore()
+    response.then((res) => {
+      assert.equal('Ciao ' + ANY_USERNAME + '. Ho tracciato la giornata di oggi.', res)
+    })
+
   })
 
   it('returns empty message if function is not founded', () => {
