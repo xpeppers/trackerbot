c0aefac2c18a1bd4bc096b7dbc8b965a2dd3ec99
Daniele Megna <daniele.megna@xpeppers.com>
2016-10-31 14:42:39 +0100

    Completed hexabot_test using sinon for mock expectations on tracker calls

--- test/hexabot_test.js
+++ test/hexabot_test.js
@@ -1,44 +1,76 @@
-'use strict';
+'use strict'
 
-const ava = require('ava');
-const requestBuilder = require('./helpers/RequestBuilder');
-const proxyquire = require('proxyquire');
+const ava = require('ava')
+const requestBuilder = require('./helpers/RequestBuilder')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 
 const TESTUSER_USERNAME = 'xpeppers.user'
-const TESTUSER_TOKEN = 'toggltoken1023jrwdfsd9v'
-const TESTUSER = { username: TESTUSER_USERNAME, project: 'phoenix', token: TESTUSER_TOKEN }
-const TODAY = require('moment')('2016-10-20')
 
 ava('track today', t => {
 
-  const momentStub = function() { return TODAY }
-  const userRepositoryStub = {
-    findFromUsername: function(username) {
-      return Promise.resolve(TESTUSER)
-    }
-  }
-  const trackerStub = function(token) {
-    return {
-      createTimeEntry: function(entry) {
-        return Promise.resolve({})
-      }
-    }
-  }
+	const expectedMorningEntry = {
+		pid: 8107914,
+		description: 'Phoenix',
+		created_with: 'TrackerBot',
+		duration: 14400,
+		billable: true,
+		start: '2016-10-20T09:00:00+02:00'
+	}
+	const expectedAfternoonEntry = {
+		pid: 8107914,
+		description: 'Phoenix',
+		created_with: 'TrackerBot',
+		duration: 14400,
+		billable: true,
+		start: '2016-10-20T14:00:00+02:00'
+	}
 
+	const bot = buildBotWithStubs(momentStub(), fakeUserRepository(), trackerStub(expectedMorningEntry, expectedAfternoonEntry))
+  const request = requestBuilder().withText('today').withUserName(TESTUSER_USERNAME)
+  const response = bot(request)
+
+  return response.then(res => {
+    t.is('Ciao ' + TESTUSER_USERNAME + '. Ho tracciato la giornata di oggi.', res)
+  })
+
+})
+
+function buildBotWithStubs(momentStub, userRepositoryStub, trackerStub) {
   momentStub['@global'] = true
   userRepositoryStub['@global'] = true
   trackerStub['@global'] = true
 
-  const bot = proxyquire('../lib', {
+  return proxyquire('../lib', {
     'moment': momentStub,
     '../user_repository': userRepositoryStub,
     '../tracker': trackerStub
   })
+}
 
-  const request = requestBuilder().withText('today').withUserName(TESTUSER_USERNAME);
-  const response = bot(request);
+function momentStub() {
+  return function() { return require('moment')('2016-10-20') }
+}
 
-  return response.then(res => {
-    t.is('Ciao ' + TESTUSER_USERNAME + '. Ho tracciato la giornata di oggi.', res);
-  });
-});
+function trackerStub(expectedMorningEntry, expectedAfternoonEntry) {
+  const tracker = require('../lib/tracker')('uselessToggleToken')
+  const trackerMock = sinon.mock(tracker)
+
+  trackerMock.expects("createTimeEntry").once().withArgs(expectedMorningEntry).returns(Promise.resolve())
+  trackerMock.expects("createTimeEntry").once().withArgs(expectedAfternoonEntry).returns(Promise.resolve())
+
+  return function(token) {
+    return tracker
+  }
+
+}
+
+function fakeUserRepository() {
+  return {
+    findFromUsername: function(username) {
+			const userFromRepository = { username: TESTUSER_USERNAME, project: 'phoenix', token: 'toggltoken1023jrwdfsd9v' }
+      return Promise.resolve(userFromRepository)
+    }
+  }
+}
+
