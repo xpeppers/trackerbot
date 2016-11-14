6fbde286af2529b4b723a53b2d46c9ba5acda5bc
Daniele Megna <daniele.megna@xpeppers.com>
2016-11-14 14:45:06 +0100

    Created BotBuilder for hexagonal tests

--- test/hexabot_test.js
+++ test/hexabot_test.js
@@ -6,13 +6,16 @@ const proxyquire = require('proxyquire')
 const sinon = require('sinon')
 
 const TESTUSER_USERNAME = 'xpeppers.user'
+const NOTEXISTING_USERNAME = 'xpeppers.user'
 const TODAY = '2016-10-20'
 
 test('track today command', t => {
   const expectedMorningEntry = entry(8107914, 'Phoenix', TODAY+'T09:00:00+02:00')
   const expectedAfternoonEntry = entry(8107914, 'Phoenix', TODAY+'T14:00:00+02:00')
 
-  const bot = buildBot([expectedMorningEntry, expectedAfternoonEntry])
+  const bot = new BotBuilder()
+    .withExpectedEntryTracked([expectedMorningEntry, expectedAfternoonEntry])
+    .build()
   const request = requestBuilder().withText('today').withUserName(TESTUSER_USERNAME)
 
   const response = bot(request)
@@ -20,11 +23,10 @@ test('track today command', t => {
   return response.then(res => {
     t.is('Ciao ' + TESTUSER_USERNAME + '. Ho tracciato la giornata di oggi.', res)
   })
-
 })
 
 test('proj command returns current project', t => {
-  const bot = buildBot()
+  const bot = new BotBuilder().build()
   const request = requestBuilder().withText('proj').withUserName(TESTUSER_USERNAME);
 
   const response = bot(request)
@@ -35,7 +37,7 @@ test('proj command returns current project', t => {
 })
 
 test.skip('set project for user', t => {
-  const bot = buildBot()
+  const bot = new BotBuilder().build()
   const request = requestBuilder().withText('proj MPOS 9871234').withUserName(TESTUSER_USERNAME);
 
   const response = bot(request)
@@ -45,20 +47,30 @@ test.skip('set project for user', t => {
   });
 });
 
-function buildBot(expectedEntriesTracked = [], date = TODAY) {
-  const momentStub = getMomentStub(date)
-  const userRepositoryStub = getFakeUserRepository()
-  const trackerStub = getTrackerStub(expectedEntriesTracked)
+function BotBuilder() {
+  this.today = TODAY
+  this.expectedEntriesTracked = []
 
-  momentStub['@global'] = true
-  userRepositoryStub['@global'] = true
-  trackerStub['@global'] = true
+  this.withExpectedEntryTracked = function(expected) {
+    this.expectedEntriesTracked = expected
+    return this
+  }
 
-  return proxyquire('../lib', {
-    'moment': momentStub,
-    '../user_repository': userRepositoryStub,
-    '../tracker': trackerStub
-  })
+  this.build = function() {
+    const momentStub = getMomentStub(this.today)
+    const userRepositoryStub = getFakeUserRepository()
+    const trackerStub = getTrackerStub(this.expectedEntriesTracked)
+
+    momentStub['@global'] = true
+    userRepositoryStub['@global'] = true
+    trackerStub['@global'] = true
+
+    return proxyquire('../lib', {
+      'moment': momentStub,
+      '../user_repository': userRepositoryStub,
+      '../tracker': trackerStub
+    })
+  }
 }
 
 function getMomentStub(date) {
@@ -105,4 +117,3 @@ function entry(pid, description, startTime) {
     start: startTime
   }
 }
-
