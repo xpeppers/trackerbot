623a4a0d7a153dbea98bb56ea2e079f27b7e3c6e
Daniele Megna <daniele.megna@xpeppers.com>
2016-12-09 15:23:36 +0100

    Added first tracker integration test
    It gets projects list from test user and workspace.
    Test credentials { "email":"test.user@toggl.com","password":"StrongPassword" }
    taken from https://github.com/toggl/toggl_api_docs/blob/master/chapters/users.md

---
+++ test/tracker_test.js
@@ -0,0 +1,15 @@
+'use strict'
+const tracker = require('../lib/tracker')
+const test = require('ava');
+
+const TESTUSER_WORKSPACE_ID = '457553'
+const TESTUSER_TOKEN = '7e7526d01db9b5d723e9a3e0943e4d99'
+
+test('list available projects on toggl', t => {
+  const toggl = tracker(TESTUSER_TOKEN)
+  const response = toggl.getWorkspaceProjects(TESTUSER_WORKSPACE_ID)
+
+  return response.then(projects => {
+    t.is(projects.length, 2)
+  })
+})
