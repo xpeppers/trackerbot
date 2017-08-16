68081b5acc6b681f9cb9d8ff693b13c57b1282d6
Daniele Megna <daniele.megna@xpeppers.com>
2017-08-16 15:23:54 +0200

    Today data in toggl bridge integration test was not mock
    Now it is fixed on 2017-01-20

--- test/toggl_bridge_test.js
+++ test/toggl_bridge_test.js
@@ -1,12 +1,16 @@
 'use strict'
-const togglBridge = require('../lib/toggl_bridge')
 const test = require('ava');
 
+/** this data could change online, check on toggl.com with this credentials:
+    - "test.user@toggl.com"
+    - "StrongPassword"
+**/
 const TESTUSER_WORKSPACE_ID = '457553'
 const TESTUSER_TOKEN = '7e7526d01db9b5d723e9a3e0943e4d99'
 
 test('list available projects on toggl', t => {
   const toggl = togglBridge(TESTUSER_TOKEN)
+
   const response = toggl.getWorkspaceProjects(TESTUSER_WORKSPACE_ID)
 
   return response.then(projects => {
@@ -20,7 +24,7 @@ test('list available projects on toggl', t => {
 })
 
 test('get last month time entries', t => {
-  const toggl = togglBridge(TESTUSER_TOKEN)
+  const toggl = togglBridgeWithTodayDate(TESTUSER_TOKEN, '2017-01-20')
 
   const response = toggl.getLastMonthTimeEntries()
 
@@ -37,3 +41,23 @@ test('get last month time entries', t => {
     t.deepEqual(entries, expected)
   })
 })
+
+function togglBridge(togglToken) {
+  const togglBridge = require('../lib/toggl_bridge')
+  return togglBridge(togglToken)
+}
+
+function togglBridgeWithTodayDate(togglToken, todayDate) {
+  const momentStub = function(stringToParse) {
+    stringToParse = stringToParse || todayDate
+    return require('moment-timezone')(stringToParse).tz('America/New_York')
+  }
+
+  momentStub['@global'] = true
+  const proxyquire = require('proxyquire')
+  const togglBridge = proxyquire('../lib/toggl_bridge', {
+    'moment-timezone': momentStub
+  })
+
+  return togglBridge(togglToken)
+}
